79dba5f4b5ab65dfabee431acdc27559d705c064
hectorelmas <124789223+hmorhid1407@users.noreply.github.com>
2023-05-24 13:13:54 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,113 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        # # . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . # # # .
+        # # . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # . # . #
+        . # # # .
+        # # . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        # . # . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
